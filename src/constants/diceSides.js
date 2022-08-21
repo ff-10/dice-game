ace29215c9ce8bf3baec8899d66b567e5e667a21
Fatali Fataliyev <fataliyev999@gmail.com>
2022-08-21 15:11:35 +0400

    diceSides.js created & images expored

---
+++ src/constants/diceSides.js
@@ -0,0 +1,17 @@
+import face1 from '../images/1.png';
+import face2 from '../images/2.png';
+import face3 from '../images/3.png';
+import face4 from '../images/4.png';
+import face5 from '../images/5.png';
+import face6 from '../images/6.png';
+
+const sides = {
+    "1": face1,
+    "2": face2,
+    "3": face3,
+    "4": face4,
+    "5": face5,
+    "6": face6,
+}
+
+export default sides;
